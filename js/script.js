69234b519f45a36410c376fd8c196ad41a7bc142
EloraDelanes <eloraginish@gmail.com>
2019-02-07 15:01:38 +0100

    fin js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-$(window).on("scroll",function(){
+$(window).on("scroll", function () {
     let positionY = $(this).scrollTop();
     console.log(positionY);
+
+    // hauteur d'une page web $(window).height
+    if (positionY > 450) {
+        $("#scrolltotop").css("opacity", 1);
+    }
+    if (positionY < 450) {
+        $("#scrolltotop").css("opacity", 0);
+    }
+
+
 })
